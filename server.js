57bb4b3d938395fa66e1ee9aacff7fe6d0b3238d
Graham Mc Allister <grahammcallister@gmail.com>
2019-08-10 14:05:53 +0200

    Migrate to npm for dependencies Font Awesome and Bootstrap

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mustacheExpress = require('mustache-express');
 var humanize = require('humanizer');
 
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Keyboard shortcut of the day', datetime: (new DateTime())});
+    res.render('index', { title: 'Keyboard shortcut of the day', datetime: (new Date())});
 })
   
 var app = express();
@@ -12,6 +12,11 @@ app.use(express.static('public'));
 app.engine('html', mustacheExpress());
 app.set('views', 'views');
 app.set('view engine', 'html');
+app.use('/js', [express.static(__dirname + '/node_modules/jquery/dist/'),
+                express.static(__dirname + '/node_modules/bootstrap/dist/js/')]);
+app.use('/css', [express.static(__dirname + '/node_modules/bootstrap/dist/css/'),
+                express.static(__dirname + '/node_modules/font-awesome/css/')]); 
+app.use('/fonts', express.static(__dirname + '/node_modules/font-awesome/fonts/'));
 app.use('/', router);
 
 app.listen(process.env.PORT || 8080);
